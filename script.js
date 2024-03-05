5d3326a3bd8fe06996e4ffbc1baa03e6df391723
Vinesh Raghvani <vineshraghvani1@gmail.com>
2024-03-04 21:36:26 -0700

    Setting Game Logic
    Setting up the initial logic of the game.

---
+++ script.js
@@ -0,0 +1,18 @@
+//We are going to store the game status element in this file so it's easy to access and use for later on.
+
+const displayStatus = document.querySelector('.game-status'); //Selects the HTML element with the class 'game-status'.
+
+let gameActive = true; //This keeps track of the status of the game.
+
+let currentPlayer = 'X'; //Keeps track of the current player. Starts initially with 'X'.
+
+let gameState = ['', '', '', '', '', '', '', '', '']; //Created an array to keep track of the game state.
+
+const winningMessage = () => `${currentPlayer} wins!`; //Displays the message for the player who wins
+
+const drawMessage = () => `Play again. This game ended in a draw!`; //Displays this message if the game ends in a draw
+
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn.`; //Displays this message after every turn to let the players know whose turn it is.
+
+displayStatus.innerHTML = currentPlayerTurn(); //This updates the inner HTML of the 'displayStatus' element with the return value of the 'currentPlayerTurn' function.
+
